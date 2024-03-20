5ce8af1c056bd034d97e9219e9ca31d9efc547fb
koeq <koe.be@web.de>
2024-03-20 09:40:07 +0100

    iterators, generators and iterables

--- 1/bag.ts
+++ 1/bag.ts
@@ -1,5 +1,35 @@
 import { NNode } from "./nnode";
 
+// js iterator:
+//
+// any object which implements a next method that returns:
+// 1. value --> the next value in the iteration
+// 2. done  --> flag that indicates if the last value in the sequence has been consumned
+//              if value is alongside done: true, it's the iterators return value
+//
+// consume iterator by repeatedly calling next()
+
+// js generators:
+//
+// alternative to iterators
+// allow to define an iterative algorithm by writing a function whose execution is not continuous
+// written using the 'function*' syntax
+// when called, do not initially execute the code -> return a sepcial type of iterator called generator
+// when value is consumed by calling the generators next method, the generator function executes until
+// it encounters the yield keyword
+
+// js iterables:
+//
+// an object is iterable if it defines its iteration behavior, such as what values are looped over
+// in a "for...of" construct.
+//
+// in order to be iterable an object must implement the @@iterator method
+// this means that the object (or one of the objects up its prototype chain) must have a property with
+// a "Symbol.iterator" key
+
+// Connection:
+// Iterables are objects that can be iterated over by implementing a Symbol.iterator method that returns an iterator object.
+
 class Bag<T> {
   private first: NNode<T> | undefined;
 
