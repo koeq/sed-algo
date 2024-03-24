f42543866fb88fa81ee2e46abf7a2b52c25205d7
koeq <koe.be@web.de>
2024-03-24 14:24:48 +0100

    add uf client

--- 1/uf/uf.ts
+++ 1/uf/uf.ts
@@ -1,3 +1,8 @@
+import fs from "fs";
+import readline from "readline";
+
+const FILE_PATH = `${__dirname}/tinyUF.txt`;
+
 class UF {
   #id: number[] = [];
   #count: number;
@@ -10,6 +15,10 @@ class UF {
     }
   }
 
+  connected(p: number, q: number): boolean {
+    return this.find(p) === this.find(q);
+  }
+
   getCount(): number {
     return this.#count;
   }
@@ -17,3 +26,40 @@ class UF {
   find(p: number) {}
   union(p: number, q: number) {}
 }
+
+async function main() {
+  let N = 0;
+  let uf: UF | undefined;
+  const fileStream = fs.createReadStream(FILE_PATH);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let lineCount = 0;
+
+  for await (const line of rl) {
+    lineCount++;
+
+    if (lineCount === 1) {
+      N = parseInt(line);
+      uf = new UF(N);
+      continue;
+    }
+
+    const splitted = line.split(" ");
+    const p = parseInt(splitted[0]);
+    const q = parseInt(splitted[1]);
+
+    if (uf?.connected(p, q)) continue;
+    uf?.union(p, q);
+    console.log(`${p} ${q}`);
+  }
+
+  console.log(`${uf?.getCount()} components`);
+
+  rl.close();
+}
+
+main();
