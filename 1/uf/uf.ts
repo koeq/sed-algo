4fd1d646b56f932ef3843cbe355c478059260202
koeq <koe.be@web.de>
2024-03-24 18:23:03 +0100

    uf quick union

--- 1/uf/uf.ts
+++ 1/uf/uf.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import readline from "readline";
 
-const FILE_PATH = `${__dirname}/tinyUF.txt`;
+const FILE_PATH = `${__dirname}/largeUF.txt`;
 
 class UF {
   #id: number[] = [];
@@ -23,22 +23,43 @@ class UF {
     return this.#count;
   }
 
+  // QUICK FIND
+  // 1
+  // find(p: number): number {
+  //   return this.#id[p];
+  // }
+
+  //  -> between n + 3 and 2n + 1
+  // union(p: number, q: number): void {
+  //   // 2
+  //   const pId = this.find(p);
+  //   const qId = this.find(q);
+
+  //   if (pId === qId) return;
+
+  //   for (let i = 0; i < this.#id.length; i++) {
+  //     // n
+  //     if (this.#id[i] === qId) {
+  //       // min: 1; max: n - 1
+  //       this.#id[i] = pId;
+  //     }
+  //   }
+
+  //   this.#count--;
+  // }
+
+  // QUICK UNION
   find(p: number): number {
-    return this.#id[p];
+    while (p !== this.#id[p]) p = this.#id[p];
+    return p;
   }
 
-  union(p: number, q: number): void {
-    const pId = this.find(p);
-    const qId = this.find(q);
+  union(p: number, q: number) {
+    const pRoot = this.find(p);
+    const qRoot = this.find(q);
 
-    if (pId === qId) return;
-
-    for (let i = 0; i < this.#id.length; i++) {
-      if (this.#id[i] === qId) {
-        this.#id[i] = pId;
-      }
-    }
-
+    if (pRoot === qRoot) return;
+    this.#id[pRoot] = qRoot;
     this.#count--;
   }
 }
